fd16fe293a8de92701c5a65305bf9855f5432576
Kenneth Davis <kennethdavis391@gmail.com>
2019-12-30 16:47:41 -0500

    monster model updated

--- mutamon-ui/src/models/monster.ts
+++ mutamon-ui/src/models/monster.ts
@@ -7,6 +7,9 @@ export class Monster{
     name: string
     wins: number
     isCurrent: boolean
+    speed: number
+    strength: number
+    defence: number
     mutations: Mutation[]
 
     constructor(monsterId: number, userId: number, level: number, name: string, wins: number, isCurrent: boolean, mutations: Mutation[]){
@@ -17,5 +20,14 @@ export class Monster{
         this.wins = wins
         this.isCurrent = isCurrent
         this.mutations = mutations
+
+        this.speed = level
+        this.strength = level
+        this.defence = level
+        for(let mutation of mutations){
+            this.speed += mutation.speed
+            this.strength += mutation.strength
+            this.defence += mutation.defence
+        }
     }
 }
