1f0004398348c189f24221a44ba455e898c6e747
Kenneth Davis <kennethdavis391@gmail.com>
2019-12-31 14:16:23 -0500

    navbar and app fixes.

--- mutamon-ui/src/App.tsx
+++ mutamon-ui/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HallOfFamePageComponent } from './components/hall-of-fame-page-component/HallOfFamePageComponent';
+import { store } from './store';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Mutamon</code> and save to reload. new text. even newer test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route path='/test' component={HallOfFamePageComponent}></Route>
+            <Route path='/' component={HallOfFamePageComponent}></Route>
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
