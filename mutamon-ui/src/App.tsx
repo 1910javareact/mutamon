0f8f7fe129cf62b30ac385925c0879d055b77631
Dane Vosmeier <dane.vosmeier@revature.net>
2019-12-31 15:01:24 -0500

    battle page layout

--- mutamon-ui/src/App.tsx
+++ mutamon-ui/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BattleComponent } from './components/battle-component/BattleComponents';
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>Mutamon</code> and save to reload. new text. even newer test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BattleComponent />
     </div>
   );
 }
