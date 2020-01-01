7d4000419d3a052cf39212005c0cdcf0c707f785
Jeffrey Chang <you@example.com>
2020-01-01 17:38:28 -0500

    asdfasdfas

--- mutamon-ui/src/App.tsx
+++ mutamon-ui/src/App.tsx
@@ -8,22 +8,6 @@ import { store } from './store';
 const App: React.FC = () => {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Mutamon</code> and save to reload. new text. even newer test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Provider store={store}>
         <Router>
           <Switch>
@@ -32,7 +16,6 @@ const App: React.FC = () => {
           </Switch>
         </Router>
       </Provider>
->>>>>>> 23822bb89dea56d3306be3ad1145cd603b92a35c
     </div>
   );
 }
