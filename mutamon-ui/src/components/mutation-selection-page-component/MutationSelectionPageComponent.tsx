5e1d39d04be74b12c563ec9eed09174d64711cb8
Kenneth Davis <kennethdavis391@gmail.com>
2020-01-02 18:09:39 -0500

    mutation selection half way done

--- mutamon-ui/src/components/mutation-selection-page-component/MutationSelectionPageComponent.tsx
+++ mutamon-ui/src/components/mutation-selection-page-component/MutationSelectionPageComponent.tsx
@@ -1,25 +1,62 @@
 import React from 'react'
 import { Mutation } from '../../models/mutation';
 import { Monster } from '../../models/monster';
+import { Redirect } from 'react-router';
+import { mutamonApiGetMutationChoices } from '../../remote/mutamon-clients/mutamon-mutations';
 
-interface MutationSelectionPageComponentProps{
+interface MutationSelectionPageComponentProps {
     currentMutamon: Monster
     updateCurrentMutamon: (monster: Monster) => void
 }
 
-interface MutationSelectionPageComponentState{
+interface MutationSelectionPageComponentState {
     mutations: Mutation[],
+    validMutation: boolean,
 }
 
 export class MutationSelectionPageComponent extends React.Component<MutationSelectionPageComponentProps, MutationSelectionPageComponentState>{
-    
-    
 
-    render(){
-        return(
-            <div>
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            mutations: [],
+            validMutation: true
+        }
+    }
 
-            </div>
+    async componentDidMount() {
+        //if the current mutamon is at a valid mutation state
+        if ((this.props.currentMutamon.level === 1 || this.props.currentMutamon.level === 3 || this.props.currentMutamon.level === 5 || this.props.currentMutamon.level === 7) &&
+            this.props.currentMutamon.mutations.length < (this.props.currentMutamon.level + 1) / 2) {
+            try {
+                let res = await mutamonApiGetMutationChoices(this.props.currentMutamon.level)
+
+                if (res.status === 200) {
+                    this.setState({
+                        ...this.state,
+                        mutations: res.body
+                    })
+                }
+            } catch (e) {
+                
+            }
+        } else {
+            this.setState({
+                ...this.state,
+                validMutation: false
+            })
+        }
+    }
+
+    render() {
+
+        return (
+            this.state.validMutation ?
+                <div>
+
+                </div>
+                :
+                <Redirect to='/test'></Redirect>
         )
     }
 }
