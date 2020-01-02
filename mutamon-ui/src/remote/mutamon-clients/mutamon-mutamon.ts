c2d5f94975eb9472e723f6f040b3461d701c51b2
Kenneth Davis <kennethdavis391@gmail.com>
2020-01-02 17:03:09 -0500

    update/post mutamon

--- mutamon-ui/src/remote/mutamon-clients/mutamon-mutamon.ts
+++ mutamon-ui/src/remote/mutamon-clients/mutamon-mutamon.ts
@@ -1,4 +1,5 @@
 import { mutamonClient } from "./mutamon-client";
+import { Monster } from "../../models/monster";
 
 export async function mutamonApiGetWinningMonstersById(userId: number){
 
@@ -27,6 +28,32 @@ export async function mutamonApiGetCurrentMonsterById(userId: number){
     try{
         const response = await mutamonClient.get('/mutamon/' + userId)
         if(response.status === 200){
+            return{
+                status: response.status,
+                body: response.data,
+                header: response.headers
+            }
+        }else{
+            return {
+                status: response.status,
+                body: undefined
+            }
+        }
+    }catch(e){
+        console.log(e);
+        throw new Error('Something Went Wrong')
+    }
+}
+
+export async function mutamonApiUpdateMonster(monster: Monster){
+    
+    const body = {
+        monster
+    }
+
+    try{
+        const response = await mutamonClient.patch('/mutamon',body)
+        if(response.status === 201){
             return{
                 status: response.status,
                 body: response.data,
