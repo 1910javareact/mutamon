a7908eaea9f18bffb4742ae3294ad95505110993
Kenneth Davis <kennethdavis391@gmail.com>
2020-01-04 23:54:02 -0500

    minor changes

--- mutamon-ui/src/remote/mutamon-clients/mutamon-client.ts
+++ mutamon-ui/src/remote/mutamon-clients/mutamon-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const mutamonClient = axios.create({
-    baseURL: 'http://3.88.86.151:8080/',
+    baseURL: 'http://54.211.15.164:1910',
     headers: {
         'Content-Type':'application/json'
     },
